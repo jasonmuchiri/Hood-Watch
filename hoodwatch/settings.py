4fd2e46a92a0e3fab4f2742b81dac46a5ffa0adf
jasonmuchiri <jasonmkinyua@gmail.com>
2019-06-06 12:57:39 +0300

    Add tinymce to the list of installed apps in settings.py

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'neighbourhood.apps.NeighbourhoodConfig',
-    'bootstrap3'
+    'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
